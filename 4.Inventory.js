3d66e809499383e315a47b5adb17f66dec4ddb48
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-10 18:58:03 +0100

    Add exercise 4

---
+++ 4.Inventory.js
@@ -0,0 +1,11 @@
+function createInventory(arr) {
+    
+}
+
+
+createInventory([
+'Isacc / 25 / Apple, GravityGun',
+'Derek / 12 / BarrelVest, DestructionSword',
+'Hes / 1 / Desolator, Sentinel, Antara'
+]
+)
