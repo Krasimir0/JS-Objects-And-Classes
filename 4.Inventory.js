55af8da27a93bde0d8887a21a095ed6e475ba0ef
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-11 13:45:19 +0100

    Finish exercise 4

--- 4.Inventory.js
+++ 4.Inventory.js
@@ -1,5 +1,19 @@
 function createInventory(arr) {
-    
+    let objOfHeroes = {};
+    let arrOfObj = [];
+    for (const hero of arr) {
+        let [heroName, heroLevel, heroItems] = hero.split(' / ')
+        arrOfObj.push({
+            Hero: heroName,
+            level: Number(heroLevel),  
+            items: heroItems
+        })
+    }
+   arrOfObj.sort((a,b) => a.level - b.level).forEach(heroObj => {
+           console.log(`Hero: ${heroObj.Hero}`);
+           console.log(`level => ${heroObj.level}`);
+           console.log(`items => ${heroObj.items}`);
+   })
 }
 
 
